adf312b08401a5e56a6f4a56403140b47040e09a
Hoà hoa hoè <97334361+tranhoa2003@users.noreply.github.com>
2023-11-11 10:19:10 +0700

    Add files via upload
    sửa css

--- DACS/config.php
+++ DACS/config.php
@@ -1,7 +1,7 @@
 <?php
     $connect = mysqli_connect("localhost","root","","vidu");
     if($connect){
-        mysqli_query($connect, "SET NAMES 'UTF8'");
+        mysqli_query($connect, "SET NAMES 'UTF8");
         // echo "Ket noi thanh cong";
     }else{
         echo "Ket noi that bai";
