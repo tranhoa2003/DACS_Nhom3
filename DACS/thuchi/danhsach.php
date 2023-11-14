f36e4fed4d30447d2083df8d287a7ac5a861d48e
Hoà hoa hoè <97334361+tranhoa2003@users.noreply.github.com>
2023-11-15 04:19:48 +0700

    Add files via upload
    sua css

--- DACS/thuchi/danhsach.php
+++ DACS/thuchi/danhsach.php
@@ -17,6 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đồ án</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="baocao.css">
     <script src="baocao.js"></script>
     <style>
         .test{
